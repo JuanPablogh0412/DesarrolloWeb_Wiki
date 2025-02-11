4baf1caaa5402da2820cf3efa09736e5626d79ab
JuanPGH0412 <juanpablogh0412@gmail.com>
2025-02-11 15:20:40 -0500

    Js formulario

--- src/main/resources/static/scriptContactenos.js
+++ src/main/resources/static/scriptContactenos.js
@@ -11,10 +11,56 @@ function revisar(){
     if(nombres.value.length == 0){
         document.getElementById("error1_nombre").hidden = false
         nombres.style.backgroundColor = "red"
+        console.log("entro 1")
 
     }
-    else if(nombres.value.length > 100){
+    if(nombres.value.length > 100){
         document.getElementById("error2_nombre").hidden = false
         nombres.style.backgroundColor = "red"
+    }
+
+    if(apellidos.value.length == 0){
+        document.getElementById("error1_apellido").hidden = false
+        apellidos.style.backgroundColor = "red"
+        console.log("entro 2")
+
+    }
+    if(apellidos.value.length > 100){
+        document.getElementById("error2_apellido").hidden = false
+        apellidos.style.backgroundColor = "red"
+    }
+    
+    if(correo.value.length == 0){
+        document.getElementById("error1_correo").hidden = false
+        correo.style.backgroundColor = "red"
+        console.log("entro 3")
+
+    }
+    //falta agregar la verificacion de caracteres especiales
+    if(!correo.value.includes("@") || !correo.value.includes(".")){
+        document.getElementById("error2_correo").hidden = false
+        correo.style.backgroundColor = "red"
+    }
+
+    if(semestre.value.length == 0){
+        document.getElementById("error1_semestre").hidden = false
+        semestre.style.backgroundColor = "red"
+        console.log("entro 4")
+
+    }
+    if(semestre.value <=0 || semestre.value >16){
+        document.getElementById("error2_semestre").hidden = false
+        semestre.style.backgroundColor = "red"
+    }
+
+    if(descripcion.value.length == 0){
+        document.getElementById("error1_descrip").hidden = false
+        semestre.style.backgroundColor = "red"
+        console.log("entro 5")
+
+    }
+    if(descripcion.value.length > 1000){
+        document.getElementById("error2_descrip").hidden = false
+        semestre.style.backgroundColor = "red"
     }
 }
